f677622fcad8a274fc368705bfb4307b1377bd47
itsGovi <govardhanyadavsangi@gmail.com>
2024-12-15 10:13:26 +0530

    was half way to solve the really "brain-scrathing" q 21. took gpt help to understand how to create reusable def's(like lambda maybe)

--- test.sql
+++ test.sql
@@ -1,24 +1,20 @@
+WITH grouped_data AS (
+  SELECT
+    is_manager,
+    department,
+    position,
+    level,
+    direct_reports,
+    ROUND(CAST(avg_base_salary AS NUMERIC), 2) AS avg_base_salary,
+    ROUND(CAST(avg_total_comp AS NUMERIC), 2) AS abg_total_comp
+  FROM employees
+  GROUP BY is_manager, department, position, level, direct_reports
+)
 SELECT
-  region,
-  is_manager,
-  employee_id,
-  department,
-  position,
-  level,
-  AVG(base_salary),
-  AVG(total_comp)
-FROM employees
-
-
-/*
-SELECT
-  employee_id,
-  full_name,
-  region,
-  position,
-  level,
-  is_manager
-FROM employees
-WHERE is_manager = 'False'
-GROUP BY 
-*/
+  g1.department,
+  g1.position,
+  g1.level,
+  g1.avg_base_salary AS manager_base_comp,
+  g2.avg_base_salary AS non_manager_base_comp,
+  ROUND(CAST(g2.avg_base_salary - g1.avg_base_salary)/ g2.avg_base_salary * 100 AS NUMERIC), 2)
+FROM
