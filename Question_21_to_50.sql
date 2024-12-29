3d1033d2cae69e3351defd79458bf2b6a8e1cd5a
itsGovi <govardhanyadavsangi@gmail.com>
2024-12-29 07:52:53 +0530

    solved q. 38, learnt there's LENGTH & REPLACE to help with count of certificates other than unnest and string to array!

--- Question_21_to_50.sql
+++ Question_21_to_50.sql
@@ -537,3 +537,20 @@ GROUP BY region, primary_specialization
 
 
 -- Question 38: Identify employees with at least two certifications and a `flight_risk` below 20%.
+
+SELECT
+    employee_id,
+    full_name,
+    department,
+    position,
+    level,
+    LENGTH(certifications) - LENGTH(REPLACE(certifications, ',', '')) + 1 AS certificate_count,
+    flight_risk
+FROM employees
+WHERE flight_risk <20
+    AND LENGTH(certifications) - LENGTH(REPLACE(certifications, ',', '')) + 1 >= 2
+GROUP BY department, position, level, employee_id, full_name
+
+
+
+-- Question 39:
--- test.sql
+++ test.sql
@@ -1,3 +1,12 @@
 SELECT
-    UNNEST()
+    employee_id,
+    full_name,
+    department,
+    position,
+    level,
+    LENGTH(certifications) - LENGTH(REPLACE(certifications, ',', '')) + 1 AS certificate_count,
+    flight_risk
 FROM employees
+WHERE flight_risk <20
+    AND LENGTH(certifications) - LENGTH(REPLACE(certifications, ',', '')) + 1 >= 2
+GROUP BY department, position, level, employee_id, full_name
