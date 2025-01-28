53d935252c190d0d0e354807cb16284984ae833e
itsGovi <govardhanyadavsangi@gmail.com>
2025-01-28 17:07:36 +0530

    got the difficulty cranked up for q2 and finished in 10 minutes!

--- Phase 2/questions 1-20.sql
+++ Phase 2/questions 1-20.sql
@@ -55,3 +55,43 @@ ORDER BY l.department, l.engagement_score DESC;
 
 
 "Q2:"
+WITH exploded_certifications AS (
+    SELECT
+        UNNEST(STRING_TO_ARRAY(certifications, ',')) AS certificate_name,
+        department,
+        region
+    FROM employees
+),
+certification_counts AS (
+    SELECT
+        certificate_name,
+        department,
+        region,
+        COUNT(*) as total_count
+    FROM exploded_certifications
+    GROUP BY department, region, certificate_name
+),
+ranked_certifications AS (
+    SELECT
+        certificate_name,
+        total_count,
+        department,
+        region,
+        RANK() OVER (PARTITION BY region ORDER BY total_count) AS regional_rank
+    FROM certification_counts
+)
+
+SELECT
+    region,
+    certificate_name,
+    total_count,
+    department
+FROM ranked_certifications
+WHERE regional_rank <= 3
+ORDER BY region, regional_rank;
+
+
+---
+
+
+"Q3:"
